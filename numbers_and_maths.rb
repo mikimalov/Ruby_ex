c2f49e3541713b634c61522bb3cef30cb4f19dae
mikimalov <60898750+mikimalov@users.noreply.github.com>
2020-02-10 21:30:22 +0100

    Add files via upload

---
+++ numbers_and_maths.rb
@@ -0,0 +1,14 @@
+puts "I will drink at friday."
+
+puts "If i drink", 20 - 30 / 2
+
+puts "I can survive."
+
+puts "If i go with", 40 - 25 / (3 * 1.0) 
+# Result is a Floating point number, only when you use floating point number like 1.0 in this case.
+puts "I will be so drunk!!"
+
+puts "I will be drunk if i drink", 35 * 2, 29 >= 25
+
+puts "I will be sober if i drink", 16 / 1.5, 88 == 89 && 22 != 22
+# Uf :D
